cf61d26bbbb8c85e43e18a4bb1df4806f88fd0c2
Alex Griffith <alexalexgriffith@gmail.com>
2019-04-24 15:59:49 -0400

    Post Lecture 03

--- 03-oo-relations-one-to-many/user.rb
+++ 03-oo-relations-one-to-many/user.rb
@@ -11,7 +11,10 @@ class User
     # Go ask the Tweet Model a question ("querying")
     # (its the responsibility of the Tweet model to know about all the tweets)
     # Look though all of the tweest in the @@all array
-    # select just the ones that belong to this particular user 
+    # select just the ones that belong to this particular user
+    Tweet.all.select do |tweet|
+      tweet.user == self
+    end
   end
 
   # user.post_tweet("coffeeeeeeeee")
