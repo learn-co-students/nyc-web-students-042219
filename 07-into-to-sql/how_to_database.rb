2c32530c04683c803e241d55a69cea478023a3b2
Alex Griffith <alexalexgriffith@gmail.com>
2019-05-01 11:15:14 -0400

    Post SQL lecture

--- 07-into-to-sql/how_to_database.rb
+++ 07-into-to-sql/how_to_database.rb
@@ -3,3 +3,17 @@ require 'pry'
 
 
 db = SQLite3::Database.new('chinook.db')
+
+# module SQLite3
+#
+#   class Database
+      # attr_accessor :results_as_hash
+
+#     def initialize(filename)
+#
+#     end
+#   end
+#
+# end
+
+binding.pry
