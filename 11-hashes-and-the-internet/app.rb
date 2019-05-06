7c486864b047840cd90e7fe59e24bdbfa225de65
Alex Griffith <alexalexgriffith@gmail.com>
2019-05-06 11:12:11 -0400

    Post Lecture 11

--- 11-hashes-and-the-internet/app.rb
+++ 11-hashes-and-the-internet/app.rb
@@ -8,9 +8,50 @@ require 'json'
 #   * https://www.googleapis.com/books/v1/volumes?q=ruby+programming
 # * Display the titles, author names, and description for each book
 
-puts "Welcome to the Book Searcher"
+Book (title, description)
+Author
+User
 
-response = RestClient.get("https://www.googleapis.com/books/v1/volumes?q=ruby+programming")
+class CLI
+
+  attr_accessor :current_user
+
+  def welcome
+    puts "Welcome to the Book Searcher"
+    puts "Enter your name"
+    username = gets.chomp
+    current_user = User.find_by(username: username)
+    
+    puts "Enter a search term to find books about:"
+    search_term = gets.chomp
+  end
+
+  def print_books
+
+  end
+
+end
+
+ClI.new.welcome
+
+
+def welcome
+  puts "Welcome to the Book Searcher"
+  puts "Enter a search term to find books about:"
+  search_term = gets.chomp
+end
+
+def print_books
+
+end
+
+
+
+puts "Welcome to the Book Searcher"
+puts "Enter a search term to find books about:"
+search_term = gets.chomp
+
+response = RestClient.get("https://www.googleapis.com/books/v1/volumes?q=#{search_term}")
 # at this point we have a string, we want to convert this string into a Hash
 string = response.body
 
@@ -20,7 +61,28 @@ books = data["items"]
 
 books.each do |book|
   title = book["volumeInfo"]["title"]
-  puts title
+  author_data = book["volumeInfo"]["authors"]
+  if author_data
+    authors = author_data.join(" & ")
+  else
+    authors = "No Authors found for this book"
+  end
+
+  description_data = book["volumeInfo"]["description"]
+
+  if description_data
+    description = description_data[0..100] + "..."
+  else
+    description = "No description found for this book"
+  end
+
+  puts "Title: #{title}"
+  puts "Authors: #{authors}"
+  puts "Description: #{description}"
+
+  # puts author names separated by an &
+
+  # puts the first 100 characters of the description followed by ...
 
   puts "*" * 15
 end
