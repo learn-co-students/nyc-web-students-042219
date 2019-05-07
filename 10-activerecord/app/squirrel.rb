a3710c4c82add73e23387fd1162a85abf6622da3
Alex Griffith <alexalexgriffith@gmail.com>
2019-05-07 09:31:02 -0400

    Add all the things

--- 10-activerecord/app/squirrel.rb
+++ 10-activerecord/app/squirrel.rb
@@ -6,8 +6,10 @@ class Squirrel < ActiveRecord::Base
 
   # has_many(:trees, {:through => :nests})
   has_many :trees, through: :nests
-  
+
   # def trees
-  #   # SELECT "trees".* FROM "trees" WHERE "trees"."squirrel_id" = ?
+ # SELECT "trees".* FROM "trees"
+ # INNER JOIN "nests" ON "trees"."id" = "nests"."tree_id"
+  # WHERE "nests"."squirrel_id" = ?
   # end
 end
