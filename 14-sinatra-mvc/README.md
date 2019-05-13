238fa3b7e346f480adbf838df47dd3308023c575
github username <github email address>
2019-05-13 14:57:14 -0400

    sinatra mvc

---
+++ 14-sinatra-mvc/README.md
@@ -0,0 +1,12 @@
+# Sinatra MVC
+
+- What is MVC?
+
+  - Model
+  - View
+  - Controller
+
+- What do we mean by Convention over Configuration?
+  - RESTful Routing
+    - "domain/resource"
+    - GET "localhost:9393/rappers"
---
+++ 14-sinatra-mvc/rappers/db/seeds.rb
@@ -0,0 +1,8 @@
+Rapper.create!(name: "Vince Staples", album_sales: 1 , fire: true )
+Rapper.create!(name: "Nas", album_sales: 1000000, fire: true )
+Rapper.create!(name: "Matty B", album_sales: 1 , fire: false )
+Rapper.create!(name: "Bushwick Bill", album_sales: 5000, fire: true )
+Rapper.create!(name: "Missy Elliott", album_sales: 1000000 , fire: true )
+Rapper.create!(name: "Lauryn Hill", album_sales: 2000000 , fire: true )
+Rapper.create!(name: "Dylan", album_sales: 0 , fire: false )
+Rapper.create!(name: "Rico Nasty", album_sales: 0 , fire: true )
