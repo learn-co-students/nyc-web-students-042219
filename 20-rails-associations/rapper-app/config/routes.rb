150696cf1b4ed17c683906b2ccdc2d0a1d27e662
github username <github email address>
2019-05-20 15:25:57 -0400

    syntax

--- 20-rails-associations/rapper-app/config/routes.rb
+++ 20-rails-associations/rapper-app/config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
 # get "/rappers", to: "rappers#index"
 resources :rappers, only: [:index, :new, :show, :create, :destroy]
-get "/rappers/:id/fire", to: "rappers#fire", as: "fire"
-delete "/rappers/:id/age", to: "rappers#delete_age"
 get "/songs", to: "songs#index", as: "songs"
 get "/songs/new", to: "songs#new"
-get "/songs/:id", to: "songs#show", as: "song"
 post "/songs", to:"songs#create"
+get "/rappers/:id/fire", to: "rappers#fire", as: "fire"
 get "/songs/:id/edit", to: "songs#edit"
+get "/songs/:id", to: "songs#show", as: "song"
 patch "/songs/:id", to: "songs#create"
 delete "/songs/:id", to: "songs#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
