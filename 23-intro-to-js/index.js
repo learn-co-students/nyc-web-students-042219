7086c4d4918a4e315c2d90e230d12273a69c7f86
Wangtron <evansjwang@gmail.com>
2019-06-03 18:10:55 -0400

    🤠 powerful

---
+++ 23-intro-to-js/index.js
@@ -0,0 +1,9 @@
+console.log('%c HELLO WORLD', 'color: green')
+console.log('%c HELLO WORLD', 'color: gold')
+debugger
+// console.lag('%c HELLO WORLD', 'color: goldenrod')
+
+function onePlusOne() {
+
+  return 1 + 'three'
+}
