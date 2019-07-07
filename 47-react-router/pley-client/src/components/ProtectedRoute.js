a30bcadf2de49e7d3ae94583d5ab4317c093b6b8
ihollander <ianholla@gmail.com>
2019-07-07 07:59:45 -0400

    fixed ProtectedRoute

--- 47-react-router/pley-client/src/components/ProtectedRoute.js
+++ 47-react-router/pley-client/src/components/ProtectedRoute.js
@@ -1,14 +1,27 @@
-import React from 'react'
-import { Route, Redirect } from 'react-router-dom'
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 
 const ProtectedRoute = props => {
-  if (props.isSignedIn) {
-    return <Route path={props.path} component={props.component} />
-  } else {
-    return (
-      <Redirect to="/" />
-    );
-  }
-}
+  // for this to work with the Switch component, it should return a Route component
+  return (
+    <Route
+      path={props.path}
+      render={routeProps => {
+        // this will determine if the component is rendered or if the user is redirected
+        if (props.isSignedIn) {
+          const Component = props.component;
+          return <Component {...routeProps} />;
+        } else {
+          return <Redirect to="/" />;
+        }
+      }}
+    />
+  );
+  // if (props.isSignedIn) {
+  //   return <Route path={props.path} component={props.component} />;
+  // } else {
+  //   return <Redirect to="/" />;
+  // }
+};
 
-export default ProtectedRoute
+export default ProtectedRoute;
